8a87de3e1c01e58b0ce1613232aaa638b7534496
Jacob Archambault <jacobarchambault@gmail.com>
2026-09-15 21:41:33 -0400

    Strings.join

--- echo1.go
+++ echo1.go
@@ -3,14 +3,10 @@ package main
 import (
 	"fmt"
 	"os"
+	"strings"
 )
 
 // Echo2 prints its command-line arguments.
 func main() {
-	s, sep := "", ""	
-	for _, arg := range os.Args[1:] { 
-		s += sep + arg
-		sep = " "
-	}
-	fmt.Println(s)
+	fmt.Println(strings.Join(os.Args[1:], " "))
 }
