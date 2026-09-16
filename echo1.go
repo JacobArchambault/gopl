48880c246952af324ff87c4be8eb1d953edd7da4
Jacob Archambault <jacobarchambault@gmail.com>
2026-09-15 21:37:19 -0400

    For loop

--- echo1.go
+++ echo1.go
@@ -5,11 +5,11 @@ import (
 	"os"
 )
 
-// Echo1 prints its command-line arguments.
+// Echo2 prints its command-line arguments.
 func main() {
-	var s, sep string
-	for i := 1; i < len(os.Args); i++ {
-		s += sep + os.Args[i]
+	s, sep := "", ""	
+	for _, arg := range os.Args[1:] { 
+		s += sep + arg
 		sep = " "
 	}
 	fmt.Println(s)
