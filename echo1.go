99c5f1d12d5173acb90c89c0a1efb6b07cff8e37
Jacob Archambault <jacobarchambault@gmail.com>
2026-09-15 21:43:30 -0400

    Print os.args directly

--- echo1.go
+++ echo1.go
@@ -3,10 +3,9 @@ package main
 import (
 	"fmt"
 	"os"
-	"strings"
 )
 
 // Echo2 prints its command-line arguments.
 func main() {
-	fmt.Println(strings.Join(os.Args[1:], " "))
+	fmt.Println(os.Args[1:])
 }
