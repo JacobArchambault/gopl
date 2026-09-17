e5dbee8154bad071573dcf6520c119118ddd4f1d
Jacob Archambault <jacobarchambault@gmail.com>
2026-09-16 21:48:48 -0400

    gofmt

--- lissajous.go
+++ lissajous.go
@@ -13,8 +13,8 @@ import (
 var palette = []color.Color{color.Black, color.RGBA{0x00, 0xff, 0x00, 0xff}, color.RGBA{0xff, 0x00, 0xff, 0xff}}
 
 const (
-	blackIndex = 0 // first color in palette
-	greenIndex = 1 // next color in palette
+	blackIndex   = 0 // first color in palette
+	greenIndex   = 1 // next color in palette
 	magentaIndex = 2
 )
 
@@ -24,11 +24,11 @@ func main() {
 
 func lissajous(out io.Writer) {
 	const (
-		cycles = 5	// number of complete x oscillator revolutions
-		res = 0.001	// angular resolution
-		size = 100	// image canvas covers [-1..+size]
-		nframes = 64	// number of animation frames
-		delay = 8	// delay between frames in 10ms units
+		cycles  = 5     // number of complete x oscillator revolutions
+		res     = 0.001 // angular resolution
+		size    = 100   // image canvas covers [-1..+size]
+		nframes = 64    // number of animation frames
+		delay   = 8     // delay between frames in 10ms units
 	)
 	freq := rand.Float64() * 3.0 // relative frequency of y oscillator
 	anim := gif.GIF{LoopCount: nframes}
@@ -40,9 +40,13 @@ func lissajous(out io.Writer) {
 			x := math.Sin(t)
 			y := math.Sin(t*freq + phase)
 			var whichIndex uint8 = 0
-			if t < cycles*math.Pi { whichIndex = magentaIndex } else {whichIndex = greenIndex }
+			if t < cycles*math.Pi {
+				whichIndex = magentaIndex
+			} else {
+				whichIndex = greenIndex
+			}
 			img.SetColorIndex(size+int(x*size+0.5), size+int(y*size+0.5), whichIndex)
-			
+
 		}
 		phase += 0.1
 		anim.Delay = append(anim.Delay, delay)
