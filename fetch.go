4cd9156c03dfe1d6c0c4c08e1b8b54009c65e09a
Jacob Archambault <jacobarchambault@gmail.com>
2026-09-23 22:15:48 -0400

    print status

--- fetch.go
+++ fetch.go
@@ -13,13 +13,14 @@ import (
 func main() {
 	for _, url := range os.Args[1:] {
 		if !strings.HasPrefix(url, "http://") && !strings.HasPrefix(url, "https://") {
-			url = "http://" + url
+			url = "https://" + url
 		}
 		resp, err := http.Get(url)
 		if err != nil {
 			fmt.Fprintf(os.Stderr, "fetch: %v\n", err)
 			os.Exit(1)
 		}
+		fmt.Println(resp.Status)
 		b, err := io.Copy(os.Stdout, resp.Body)
 		resp.Body.Close()
 		if err != nil {
